58954a122765fa9cacf5b89cb9f10f17937937b0
git-chad <tobimocc@gmail.com>
2023-06-29 19:44:07 -0300

    added scroll-to-top btn

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -4,13 +4,18 @@ import CharacterGrid from "../components/characters/character-grid.component";
 import Pagination from "../components/pagination/pagination.component";
 import { useAppSelector, useAppDispatch } from "../store/hooks";
 import { fetchCharacters } from "../store/characterReducer";
-import { fetchFavoriteCharacters, fetchResetFavorites } from "../store/favoritesReducer";
+import {fetchFavoriteCharacters, fetchResetFavorites,} from "../store/favoritesReducer";
 import FavResetConfirmation from "../components/modals/FavResetConfirmation";
+import { animateScroll } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
 
 const Homepage = () => {
   const characters = useAppSelector((state) => state.characters.characters);
   const dispatch = useAppDispatch();
   const [showAlert, setShowAlert] = useState(false);
+  const [showScrollButton, setShowScrollButton] = useState(false);
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
@@ -28,7 +33,26 @@ const Homepage = () => {
     setShowAlert(false);
   };
 
-  const isButtonDisabled = useAppSelector((state) => state.favorites.list.length === 0);
+  const isButtonDisabled = useAppSelector(
+    (state) => state.favorites.list.length === 0
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollToTop = window.scrollY;
+      const showButton = scrollToTop > 400; // Show button when scrolled 200 pixels or more
+      setShowScrollButton(showButton);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    animateScroll.scrollToTop();
+  };
 
   return (
     <div className="container-l">
@@ -36,7 +60,7 @@ const Homepage = () => {
         <h3 className="text-2xl font-bold">Character Catalog</h3>
         <button
           className="danger hover:bg-red-700 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-          disabled={isButtonDisabled}  
+          disabled={isButtonDisabled}
           onClick={handleRemoveAllFavorites}
         >
           Remove all
@@ -51,6 +75,14 @@ const Homepage = () => {
           onConfirm={confirmRemoveAllFavorites}
           onCancel={cancelRemoveAllFavorites}
         />
+      )}
+      {showScrollButton && (
+        <button
+        className="fixed bottom-4 right-4 z-10 p-2 bg-gray-300 rounded-full hover:bg-gray-400 focus:outline-none"
+        onClick={scrollToTop}
+      >
+        <FontAwesomeIcon icon={faArrowUp} />
+      </button>
       )}
     </div>
   );
